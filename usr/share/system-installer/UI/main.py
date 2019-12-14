cc37e43804b8d006d755e4921b355811162dd78a
Thomas Castleman <contact@draugeros.org>
2019-12-13 21:53:21 -0500

    progress on UI overhaul, re-enable window decorations
     * user set up has now been integrated
     * working on completion indicators
     * add clear_window() function to make repopulating windows easier
     * re-enabled window decorations, connected exit button to exit function in main class

--- usr/share/system-installer/UI/main.py
+++ usr/share/system-installer/UI/main.py
@@ -24,6 +24,37 @@
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk
+import re
+
+def hasnumbers(inputString):
+	return any(char.isdigit() for char in inputString)
+
+def hasletters(inputString):
+	if re.search('[a-zA-Z]', inputString):
+		return True
+	else:
+		return False
+
+def hasSC(inputString):
+	regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+	if regex.search(inputString) == None:
+		return False
+	else:
+		return True
+
+def hasspace(inputString):
+	x=0
+	for a in inputString:
+		if (a.isspace()) == True:
+			x=1
+			break
+		else:
+			x=0
+	if x == 1:
+		return True
+	else:
+		return False
+
 
 default = """
 	Welcome to the Drauger OS System Installer!
@@ -43,6 +74,11 @@ default = """
 	The Drauger OS System Installer is currently in alpha.
 	Expect bugs.
 	"""
+keyboard_completion = "TO DO"
+user_completion = "TO DO"
+part_completion = "TO DO"
+locale_completion = "TO DO"
+options_completion = "TO DO"
 
 class main(Gtk.Window):
 	def __init__(self):
@@ -56,7 +92,7 @@ class main(Gtk.Window):
 		self.grid.attach(self.label, 1, 1, 3, 1)
 
 		self.button1 = Gtk.Button.new_with_label("Okay -->")
-		self.button1.connect("clicked", self.onnext1clicked)
+		self.button1.connect("clicked", self.main_menu)
 		self.grid.attach(self.button1, 3, 2, 1, 1)
 
 		self.button2 = Gtk.Button.new_with_label("Exit")
@@ -129,50 +165,209 @@ class main(Gtk.Window):
 		filter_any.add_pattern("*")
 		dialog.add_filter(filter_any)
 
-	def onnext1clicked(self,button):
-		clear_window(self.grid)
+	def main_menu(self,button):
+		self.clear_window()
 
 		self.label.set_markup("""
 		Feel free to complete any of the below segments in any order.\t
-		However, all segments must be completed.""")
-		self.grid.attach(self.label, 2, 1, 1, 1)
+		However, all segments must be completed.\n""")
+		self.grid.attach(self.label, 2, 1, 2, 1)
+
+		self.completion_label = Gtk.Label()
+		self.completion_label.set_markup("""COMPLETION""")
+		self.grid.attach(self.completion_label, 2, 2, 1, 1)
 
 		self.button8 = Gtk.Button.new_with_label("Keyboard")
 		self.button8.connect("clicked",self.keyboard)
-		self.grid.attach(self.button8, 2, 2, 1, 1)
+		self.grid.attach(self.button8, 3, 3, 1, 1)
+
+		self.label_keyboard = Gtk.Label()
+		self.label_keyboard.set_markup(keyboard_completion)
+		self.grid.attach(self.label_keyboard, 2, 3, 1, 1)
 
 		self.button4 = Gtk.Button.new_with_label("Locale and Time")
 		self.button4.connect("clicked",self.locale)
-		self.grid.attach(self.button4, 2, 3, 1, 1)
+		self.grid.attach(self.button4, 3, 4, 1, 1)
+
+		self.label_locale = Gtk.Label()
+		self.label_locale.set_markup(locale_completion)
+		self.grid.attach(self.label_locale, 2, 4, 1, 1)
 
 		self.button5 = Gtk.Button.new_with_label("Options")
 		self.button5.connect("clicked",self.options)
-		self.grid.attach(self.button5, 2, 4, 1, 1)
+		self.grid.attach(self.button5, 3, 5, 1, 1)
+
+		self.label_options = Gtk.Label()
+		self.label_options.set_markup(options_completion)
+		self.grid.attach(self.label_options, 2, 5, 1, 1)
 
 		self.button6 = Gtk.Button.new_with_label("Partitioning")
 		self.button6.connect("clicked",self.partitioning)
-		self.grid.attach(self.button6, 2, 5, 1, 1)
+		self.grid.attach(self.button6, 3, 6, 1, 1)
+
+		self.label_part = Gtk.Label()
+		self.label_part.set_markup(part_completion)
+		self.grid.attach(self.label_part, 2, 6, 1, 1)
 
 		self.button7 = Gtk.Button.new_with_label("User Settings")
 		self.button7.connect("clicked",self.user)
-		self.grid.attach(self.button7, 2, 6, 1, 1)
+		self.grid.attach(self.button7, 3, 7, 1, 1)
+
+		self.label_user = Gtk.Label()
+		self.label_user.set_markup(user_completion)
+		self.grid.attach(self.label_user, 2, 7, 1, 1)
 
 		self.button1.set_label("DONE")
 		self.button1.connect("clicked",self.done)
-		self.grid.attach(self.button1, 3, 7, 1, 1)
+		self.grid.attach(self.button1, 4, 8, 1, 1)
 
-		self.grid.attach(self.button2, 1, 7, 1, 1)
+		self.grid.attach(self.button2, 1, 8, 1, 1)
 
 		self.show_all()
 
 	def user(self,button):
-		print("USER")
+		self.clear_window()
+
+		self.label = Gtk.Label()
+		self.label.set_markup("""
+	<b>Set Up Main User</b>
+		""")
+		self.label.set_justify(Gtk.Justification.CENTER)
+		self.grid.attach(self.label, 1, 1, 2, 1)
+
+		self.label1 = Gtk.Label()
+		self.label1.set_markup("	Username:	")
+		self.label1.set_justify(Gtk.Justification.LEFT)
+		self.grid.attach(self.label1, 1, 3, 1, 1)
+
+		self.username = Gtk.Entry()
+		self.grid.attach(self.username, 2, 3, 1, 1)
+
+		self.label2 = Gtk.Label()
+		self.label2.set_markup("	Computer\'s Name:	")
+		self.label2.set_justify(Gtk.Justification.LEFT)
+		self.grid.attach(self.label2, 1, 4, 1, 1)
+
+		self.compname = Gtk.Entry()
+		self.grid.attach(self.compname, 2, 4, 1, 1)
+
+		self.label3 = Gtk.Label()
+		self.label3.set_markup("	Password:	")
+		self.label3.set_justify(Gtk.Justification.LEFT)
+		self.grid.attach(self.label3, 1, 5, 1, 1)
+
+		self.password = Gtk.Entry()
+		self.password.set_visibility(False)
+		self.grid.attach(self.password, 2, 5, 1, 1)
+
+		self.label4 = Gtk.Label()
+		self.label4.set_markup("	Confirm Pasword:	")
+		self.label4.set_justify(Gtk.Justification.LEFT)
+		self.grid.attach(self.label4, 1, 6, 1, 1)
+
+		self.passconf = Gtk.Entry()
+		self.passconf.set_visibility(False)
+		self.grid.attach(self.passconf, 2, 6, 1, 1)
+
+		self.button1 = Gtk.Button.new_with_label("Okay -->")
+		self.button1.connect("clicked", self.onnext2clicked)
+		self.grid.attach(self.button1, 2, 8, 1, 1)
+
+		self.button2 = Gtk.Button.new_with_label("Exit")
+		self.button2.connect("clicked", self.exit)
+		self.grid.attach(self.button2, 1, 8, 1, 1)
+
+		self.label5 = None
+
+		self.show_all()
+
+	def onnext2clicked(self,button):
+		self.password_setting = self.password.get_text()
+		pass2 = self.passconf.get_text()
+		self.username_setting = self.username.get_text()
+		self.username_setting = self.username_setting.lower()
+		self.compname_setting = self.compname.get_text()
+		if self.password_setting != pass2:
+			if self.label5 != None:
+				self.grid.remove(self.label5)
+			self.label5 = Gtk.Label()
+			self.label5.set_markup("Passwords do not match")
+			self.label5.set_justify(Gtk.Justification.CENTER)
+			self.grid.attach(self.label5, 1, 7, 2, 1)
+		elif hasnumbers(self.password_setting) != True:
+			if self.label5 != None:
+				self.grid.remove(self.label5)
+			self.label5 = Gtk.Label()
+			self.label5.set_markup("Password contains no numbers")
+			self.label5.set_justify(Gtk.Justification.CENTER)
+			self.grid.attach(self.label5, 1, 7, 2, 1)
+		elif hasletters(self.password_setting) != True:
+			if self.label5 != None:
+				self.grid.remove(self.label5)
+			self.label5 = Gtk.Label()
+			self.label5.set_markup("Password contains no letters")
+			self.label5.set_justify(Gtk.Justification.CENTER)
+			self.grid.attach(self.label5, 1, 7, 2, 1)
+		elif len(self.password_setting) < 4:
+			if self.label5 != None:
+				self.grid.remove(self.label5)
+			self.label5 = Gtk.Label()
+			self.label5.set_markup("Password is less than 4 characters")
+			self.label5.set_justify(Gtk.Justification.CENTER)
+			self.grid.attach(self.label5, 1, 7, 2, 1)
+		elif hasSC(self.username_setting):
+			if self.label5 != None:
+				self.grid.remove(self.label5)
+			self.label5 = Gtk.Label()
+			self.label5.set_markup("Username contains special characters")
+			self.label5.set_justify(Gtk.Justification.CENTER)
+			self.grid.attach(self.label5, 1, 7, 2, 1)
+		elif hasspace(self.username_setting):
+			if self.label5 != None:
+				self.grid.remove(self.label5)
+			self.label5 = Gtk.Label()
+			self.label5.set_markup("Username contains space")
+			self.label5.set_justify(Gtk.Justification.CENTER)
+			self.grid.attach(self.label5, 1, 7, 2, 1)
+		elif len(self.username_setting) < 1:
+			if self.label5 != None:
+				self.grid.remove(self.label5)
+			self.label5 = Gtk.Label()
+			self.label5.set_markup("Username empty")
+			self.label5.set_justify(Gtk.Justification.CENTER)
+			self.grid.attach(self.label5, 1, 7, 2, 1)
+		elif hasSC(self.compname_setting):
+			if self.label5 != None:
+				self.grid.remove(self.label5)
+			self.label5 = Gtk.Label()
+			self.label5.set_markup("Computer Name contains non-hyphen special character")
+			self.label5.set_justify(Gtk.Justification.CENTER)
+			self.grid.attach(self.label5, 1, 7, 2, 1)
+		elif hasspace(self.compname_setting):
+			if self.label5 != None:
+				self.grid.remove(self.label5)
+			self.label5 = Gtk.Label()
+			self.label5.set_markup("Computer Name contains space")
+			self.label5.set_justify(Gtk.Justification.CENTER)
+			self.grid.attach(self.label5, 1, 7, 2, 1)
+		elif len(self.compname_setting) < 1:
+			if self.label5 != None:
+				self.grid.remove(self.label5)
+			self.label5 = Gtk.Label()
+			self.label5.set_markup("Computer Name is empty")
+			self.label5.set_justify(Gtk.Justification.CENTER)
+			self.grid.attach(self.label5, 1, 7, 2, 1)
+		else:
+			user_completion = "COMPLETED"
+			self.main_menu("clicked")
+
+		self.show_all()
 
 	def partitioning(self,button):
 		print("PART")
 
 	def options(self,button):
-		clear_window(self.grid)
+		self.clear_window()
 
 		self.label.set_markup("""
 	<b>Extra Options</b>
@@ -184,7 +379,7 @@ class main(Gtk.Window):
 
 		self.label1 = Gtk.Label()
 		self.label1.set_markup("""
-		Install third-party packages such as NVIDIA drivers if necessary""")
+		Install third-party packages such as NVIDIA drivers if necessary\t\t""")
 		self.label1.set_justify(Gtk.Justification.LEFT)
 		self.grid.attach(self.label1, 2, 2, 1, 1)
 
@@ -230,7 +425,8 @@ class main(Gtk.Window):
 			self.login_setting = 1
 		else:
 			self.login_setting = 0
-		self.onnext1clicked("clicked")
+		options_completion = "COMPLETED"
+		self.main_menu("clicked")
 
 	def locale(self,button):
 		print("LOCALE AND TIME")
@@ -245,21 +441,22 @@ class main(Gtk.Window):
 		exit(0)
 
 	def exit(self,button):
+		Gtk.main_quit("delete-event")
 		print(1)
 		exit(1)
 
-def clear_window(window):
-	children = Gtk.Container.get_children(window)
-	for each in children:
-		Gtk.Widget.destroy(each)
+	def clear_window(self):
+		children = self.grid.get_children()
+		for each in children:
+			self.grid.remove(each)
 
 
 def show_main():
 	window = main()
-	window.set_decorated(False)
+	window.set_decorated(True)
 	window.set_resizable(False)
 	window.set_position(Gtk.WindowPosition.CENTER)
-	window.connect("delete-event", Gtk.main_quit)
+	window.connect("delete-event", main.exit)
 	window.show_all()
 	Gtk.main()
 
