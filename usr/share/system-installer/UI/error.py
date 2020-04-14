06d8a0b7b2a9038ab7e99666bc05fa3c7c2908f5
Thomas Castleman <contact@draugeros.org>
2020-04-14 12:40:27 -0400

    allow sending installation reports from error dialog

--- usr/share/system-installer/UI/error.py
+++ usr/share/system-installer/UI/error.py
@@ -25,6 +25,10 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 from sys import argv
+from subprocess import Popen, check_output, PIPE, STDOUT
+from os import remove, listdir, getenv
+from datetime import datetime
+import report
 
 display = str(argv[1])
 
@@ -35,19 +39,77 @@ class main(Gtk.Window):
 		self.grid=Gtk.Grid(orientation=Gtk.Orientation.VERTICAL)
 		self.add(self.grid)
 		self.set_icon_from_file("/usr/share/icons/Drauger/720x720/Menus/install-drauger.png")
+		self.scrolling = False
+		self.opt_setting = False
+		self.cpu_setting = False
+		self.gpu_setting = False
+		self.ram_setting = False
+		self.disk_setting = False
+		self.log_setting = False
+		self.custom_setting = False
+		self.main_menu("clicked")
+
+	def main_menu(self, widget):
+		self.clear_window()
 
 		self.label = Gtk.Label()
-		self.label.set_markup(display)
+		self.label.set_markup("<b>" + display  + "</b>")
 		self.label.set_justify(Gtk.Justification.CENTER)
 		self.grid.attach(self.label, 1, 1, 3, 1)
 
+		self.label2 = Gtk.Label()
+		self.label2.set_markup("""
+	If you wish to notify the developers of this failed installation,\t\t
+	you can send an installation report below.
+	""")
+		self.label2.set_justify(Gtk.Justification.CENTER)
+		self.grid.attach(self.label2, 1, 2, 3, 1)
 
 		self.button2 = Gtk.Button.new_with_label("Exit")
-		self.button2.connect("clicked", self.onexitclicked)
-		self.grid.attach(self.button2, 1, 2, 1, 1)
+		self.button2.connect("clicked", self.exit)
+		self.grid.attach(self.button2, 1, 3, 1, 1)
 
-	def onexitclicked(self,button):
+		self.button = Gtk.Button.new_with_label("Send Installation report")
+		self.button.connect("clicked", self.main)
+		self.grid.attach(self.button, 3, 3, 1, 1)
+
+		self.show_all()
+
+	def clear_window(self):
+		children = self.grid.get_children()
+		for each in children:
+			self.grid.remove(each)
+		if (self.scrolling):
+			self.scrolled_window.remove(self.grid)
+			self.remove(self.scrolled_window)
+			self.add(self.grid)
+			self.scrolling = False
+			self.set_default_size(-1, -1)
+
+	def exit(self,button):
+		Gtk.main_quit("delete-event")
 		exit(0)
+
+main.main = report.main.main
+main.toggle_UI = report.main.toggle_UI
+main.message_accept = report.main.message_accept
+main.message_handler = report.main.message_handler
+main.generate_message = report.main.generate_message
+main.preview_message = report.main.preview_message
+main.send_report = report.main.send_report
+main.cpu_explaination = report.main.cpu_explaination
+main.cpu_toggle = report.main.cpu_toggle
+main.disk_explaination = report.main.disk_explaination
+main.disk_toggle = report.main.disk_toggle
+main.generate_message = report.main.generate_message
+main.gpu_explaination = report.main.gpu_explaination
+main.gpu_toggle = report.main.gpu_toggle
+main.log_explaination = report.main.log_explaination
+main.log_toggle = report.main.log_toggle
+main.ram_explaination = report.main.ram_explaination
+main.ram_toggle = report.main.ram_toggle
+main.toggle_UI = report.main.toggle_UI
+main.main = report.main.main
 
 def show_main():
 	window = main()
