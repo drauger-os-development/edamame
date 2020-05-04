7273d0b4d20bad7438e384e0fa346e3e487c0093
Thomas Castleman <contact@draugeros.org>
2020-05-04 01:44:24 -0400

    fix logic error in __update__
    cannot open a file as read/write, must be one or the other

--- usr/share/system-installer/modules/master.py
+++ usr/share/system-installer/modules/master.py
@@ -59,14 +59,19 @@ def check_internet():
         return False
 
 def __update__(percentage):
+    value = ""
     try:
-        with open("/tmp/system-installer-progress.log", "rw+") as progress:
-            if int(percentage) > int(progress.read()):
+        with open("/tmp/system-installer-progress.log", "r") as progress:
+            value  = int(progress.read())
+        if int(percentage) > value:
+            with open("/tmp/system-installer-progress.log", "w+") as progress:
                 progress.write(str(percentage))
     except PermissionError:
         chmod("/tmp/system-installer-progress.log", 0o666)
-        with open("/tmp/system-installer-progress.log", "rw+") as progress:
-            if int(percentage) > int(progress.read()):
+        with open("/tmp/system-installer-progress.log", "r") as progress:
+            value  = int(progress.read())
+        if int(percentage) > value:
+            with open("/tmp/system-installer-progress.log", "w+") as progress:
                 progress.write(str(percentage))
 
 class MainInstallation():
