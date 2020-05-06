26662d0eef9d67f5eeeda5e259387fd6f197dfaf
Thomas Castleman <contact@draugeros.org>
2020-05-06 19:42:25 -0400

    multiple changes
     * better PEP8 conformance
     *  replace range(len()) with enumerate()
     * take advantage of negative indexing

--- usr/share/system-installer/UI/confirm.py
+++ usr/share/system-installer/UI/confirm.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-#  user.py
+#  confirm.py
 #
 #  Copyright 2020 Thomas Castleman <contact@draugeros.org>
 #
@@ -30,7 +30,7 @@ SETTINGS = argv[1]
 SETTINGS = SETTINGS.split(" , ")
 
 def eprint(*args, **kwargs):
-	print(*args, file=sys.stderr, **kwargs)
+    print(*args, file=sys.stderr, **kwargs)
 
 # Partitioning settings
 AUTO_PART = SETTINGS[0]
@@ -58,214 +58,214 @@ MODEL = SETTINGS[13]
 LAYOUT = SETTINGS[14]
 VARIENT = SETTINGS[15]
 if EXTRAS != "0" and EXTRAS != None:
-	EXTRAS = "Yes"
+    EXTRAS = "Yes"
 else:
-	EXTRAS = "No"
+    EXTRAS = "No"
 if UPDATES != "0" and UPDATES != None:
-	UPDATES = "Yes"
+    UPDATES = "Yes"
 else:
-	UPDATES = "No"
+    UPDATES = "No"
 if LOGIN != "0" and LOGIN != None:
-	LOGIN = "Yes"
+    LOGIN = "Yes"
 else:
-	LOGIN = "No"
+    LOGIN = "No"
 
 class main(Gtk.Window):
 
-	def __init__(self):
-		Gtk.Window.__init__(self, title="System Installer")
-		self.grid=Gtk.Grid(orientation=Gtk.Orientation.VERTICAL)
-		self.add(self.grid)
-		self.set_icon_from_file("/usr/share/icons/Drauger/720x720/Menus/install-drauger.png")
+    def __init__(self):
+        Gtk.Window.__init__(self, title="System Installer")
+        self.grid=Gtk.Grid(orientation=Gtk.Orientation.VERTICAL)
+        self.add(self.grid)
+        self.set_icon_from_file("/usr/share/icons/Drauger/720x720/Menus/install-drauger.png")
 
-		self.label = Gtk.Label()
-		self.label.set_markup("""
-	<b>FINAL CONFIRMATION</b>
-	Please read the below summary carefully.
-	This is your final chance to cancel installation.\t
-		""")
-		self.label.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label, 1, 1, 3, 1)
+        self.label = Gtk.Label()
+        self.label.set_markup("""
+    <b>FINAL CONFIRMATION</b>
+    Please read the below summary carefully.
+    This is your final chance to cancel installation.\t
+        """)
+        self.label.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label, 1, 1, 3, 1)
 
-		self.label1 = Gtk.Label()
-		self.label1.set_markup("""
-	<b>PARTITIONS</b>
-		""")
-		self.label1.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label1, 1, 2, 3, 1)
+        self.label1 = Gtk.Label()
+        self.label1.set_markup("""
+    <b>PARTITIONS</b>
+        """)
+        self.label1.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label1, 1, 2, 3, 1)
 
-		self.label4 = Gtk.Label()
-		self.label4.set_markup("""	Partitioning:	""")
-		self.label4.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label4, 1, 4, 1, 1)
+        self.label4 = Gtk.Label()
+        self.label4.set_markup("""  Partitioning:   """)
+        self.label4.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label4, 1, 4, 1, 1)
 
-		self.label5 = Gtk.Label()
+        self.label5 = Gtk.Label()
 
-		if (AUTO_PART == "True"):
-			self.label5.set_markup("""AUTO PARTITIONING ENABLED\t
+        if (AUTO_PART == "True"):
+            self.label5.set_markup("""AUTO PARTITIONING ENABLED\t
 INSTALLATION DRIVE: %s""" % (ROOT))
-		else:
-			self.label5.set_markup("""ROOT: %s
+        else:
+            self.label5.set_markup("""ROOT: %s
 EFI: %s
 HOME: %s
 SWAP: %s""" % (ROOT,EFI,HOME,SWAP))
 
-		self.label5.set_justify(Gtk.Justification.LEFT)
-		self.grid.attach(self.label5, 3, 4, 1, 1)
+        self.label5.set_justify(Gtk.Justification.LEFT)
+        self.grid.attach(self.label5, 3, 4, 1, 1)
 
-		self.label6 = Gtk.Label()
-		self.label6.set_markup("""
-	<b>SYSTEM</b>
-		""")
-		self.label6.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label6, 1, 5, 3, 1)
+        self.label6 = Gtk.Label()
+        self.label6.set_markup("""
+    <b>SYSTEM</b>
+        """)
+        self.label6.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label6, 1, 5, 3, 1)
 
-		self.label7 = Gtk.Label()
-		self.label7.set_markup("""	Language:	""")
-		self.label7.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label7, 1, 6, 1, 1)
+        self.label7 = Gtk.Label()
+        self.label7.set_markup("""  Language:   """)
+        self.label7.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label7, 1, 6, 1, 1)
 
-		self.label8 = Gtk.Label()
-		self.label8.set_markup(LANG)
-		self.label8.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label8, 3, 6, 1, 1)
+        self.label8 = Gtk.Label()
+        self.label8.set_markup(LANG)
+        self.label8.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label8, 3, 6, 1, 1)
 
-		self.label9 = Gtk.Label()
-		self.label9.set_markup("""	Time Zone:	""")
-		self.label9.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label9, 1, 7, 1, 1)
+        self.label9 = Gtk.Label()
+        self.label9.set_markup("""  Time Zone:  """)
+        self.label9.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label9, 1, 7, 1, 1)
 
-		self.label10 = Gtk.Label()
-		self.label10.set_markup(TIME_ZONE)
-		self.label10.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label10, 3, 7, 1, 1)
+        self.label10 = Gtk.Label()
+        self.label10.set_markup(TIME_ZONE)
+        self.label10.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label10, 3, 7, 1, 1)
 
-		self.label11 = Gtk.Label()
-		self.label11.set_markup("	Computer Name:	")
-		self.label11.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label11, 1, 8, 1, 1)
+        self.label11 = Gtk.Label()
+        self.label11.set_markup("   Computer Name:  ")
+        self.label11.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label11, 1, 8, 1, 1)
 
-		self.label12 = Gtk.Label()
-		self.label12.set_markup(COMPNAME)
-		self.label12.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label12, 3, 8, 1, 1)
+        self.label12 = Gtk.Label()
+        self.label12.set_markup(COMPNAME)
+        self.label12.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label12, 3, 8, 1, 1)
 
-		self.label13 = Gtk.Label()
-		self.label13.set_markup("""
-	<b>USER</b>
-		""")
-		self.label13.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label13, 1, 9, 3, 1)
+        self.label13 = Gtk.Label()
+        self.label13.set_markup("""
+    <b>USER</b>
+        """)
+        self.label13.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label13, 1, 9, 3, 1)
 
-		self.label14 = Gtk.Label()
-		self.label14.set_markup("""	Username:	""")
-		self.label14.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label14, 1, 10, 1, 1)
+        self.label14 = Gtk.Label()
+        self.label14.set_markup(""" Username:   """)
+        self.label14.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label14, 1, 10, 1, 1)
 
-		self.label15 = Gtk.Label()
-		self.label15.set_markup(USERNAME)
-		self.label15.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label15, 3, 10, 1, 1)
+        self.label15 = Gtk.Label()
+        self.label15.set_markup(USERNAME)
+        self.label15.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label15, 3, 10, 1, 1)
 
-		self.label16 = Gtk.Label()
-		self.label16.set_markup("""	Password:	""")
-		self.label16.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label16, 1, 11, 1, 1)
+        self.label16 = Gtk.Label()
+        self.label16.set_markup(""" Password:   """)
+        self.label16.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label16, 1, 11, 1, 1)
 
-		self.label17 = Gtk.Label()
-		self.label17.set_markup(PASS)
-		self.label17.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label17, 3, 11, 1, 1)
+        self.label17 = Gtk.Label()
+        self.label17.set_markup(PASS)
+        self.label17.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label17, 3, 11, 1, 1)
 
-		self.label23 = Gtk.Label()
-		self.label23.set_markup("""	Auto-Login:	""")
-		self.label23.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label23, 1, 12, 1, 1)
+        self.label23 = Gtk.Label()
+        self.label23.set_markup(""" Auto-Login: """)
+        self.label23.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label23, 1, 12, 1, 1)
 
-		self.label24 = Gtk.Label()
-		self.label24.set_markup(LOGIN)
-		self.label24.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label24, 3, 12, 1, 1)
+        self.label24 = Gtk.Label()
+        self.label24.set_markup(LOGIN)
+        self.label24.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label24, 3, 12, 1, 1)
 
-		self.label18 = Gtk.Label()
-		self.label18.set_markup("""
-	<b>OTHER</b>
-		""")
-		self.label18.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label18, 1, 13, 3, 1)
+        self.label18 = Gtk.Label()
+        self.label18.set_markup("""
+    <b>OTHER</b>
+        """)
+        self.label18.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label18, 1, 13, 3, 1)
 
-		self.label19 = Gtk.Label()
-		self.label19.set_markup("""	Install Extras:	""")
-		self.label19.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label19, 1, 14, 1, 1)
+        self.label19 = Gtk.Label()
+        self.label19.set_markup(""" Install Extras: """)
+        self.label19.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label19, 1, 14, 1, 1)
 
-		self.label20 = Gtk.Label()
-		self.label20.set_markup(EXTRAS)
-		self.label20.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label20, 3, 14, 1, 1)
+        self.label20 = Gtk.Label()
+        self.label20.set_markup(EXTRAS)
+        self.label20.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label20, 3, 14, 1, 1)
 
-		self.label21 = Gtk.Label()
-		self.label21.set_markup("""	Install Updates:	""")
-		self.label21.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label21, 1, 15, 1, 1)
+        self.label21 = Gtk.Label()
+        self.label21.set_markup(""" Install Updates:    """)
+        self.label21.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label21, 1, 15, 1, 1)
 
-		self.label22 = Gtk.Label()
-		self.label22.set_markup(UPDATES)
-		self.label22.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label22, 3, 15, 1, 1)
+        self.label22 = Gtk.Label()
+        self.label22.set_markup(UPDATES)
+        self.label22.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label22, 3, 15, 1, 1)
 
-		self.label25 = Gtk.Label()
-		self.label25.set_markup("""	Keyboard Model:	""")
-		self.label25.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label25, 1, 16, 1, 1)
+        self.label25 = Gtk.Label()
+        self.label25.set_markup(""" Keyboard Model: """)
+        self.label25.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label25, 1, 16, 1, 1)
 
-		self.label26 = Gtk.Label()
-		self.label26.set_markup(MODEL)
-		self.label26.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label26, 3, 16, 1, 1)
+        self.label26 = Gtk.Label()
+        self.label26.set_markup(MODEL)
+        self.label26.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label26, 3, 16, 1, 1)
 
-		self.label27 = Gtk.Label()
-		self.label27.set_markup("""	Keyboard Layout:	""")
-		self.label27.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label27, 1, 17, 1, 1)
+        self.label27 = Gtk.Label()
+        self.label27.set_markup(""" Keyboard Layout:    """)
+        self.label27.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label27, 1, 17, 1, 1)
 
-		self.label28 = Gtk.Label()
-		self.label28.set_markup(LAYOUT)
-		self.label28.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label28, 3, 17, 1, 1)
+        self.label28 = Gtk.Label()
+        self.label28.set_markup(LAYOUT)
+        self.label28.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label28, 3, 17, 1, 1)
 
-		self.label29 = Gtk.Label()
-		self.label29.set_markup("""	Keyboard Varient:	""")
-		self.label29.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label29, 1, 18, 1, 1)
+        self.label29 = Gtk.Label()
+        self.label29.set_markup(""" Keyboard Varient:   """)
+        self.label29.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label29, 1, 18, 1, 1)
 
-		self.label30 = Gtk.Label()
-		self.label30.set_markup(VARIENT)
-		self.label30.set_justify(Gtk.Justification.CENTER)
-		self.grid.attach(self.label30, 3, 18, 1, 1)
+        self.label30 = Gtk.Label()
+        self.label30.set_markup(VARIENT)
+        self.label30.set_justify(Gtk.Justification.CENTER)
+        self.grid.attach(self.label30, 3, 18, 1, 1)
 
-		self.button1 = Gtk.Button.new_with_label("INSTALL NOW -->")
-		self.button1.connect("clicked", self.onnextclicked)
-		self.grid.attach(self.button1, 3, 19, 1, 1)
+        self.button1 = Gtk.Button.new_with_label("INSTALL NOW -->")
+        self.button1.connect("clicked", self.onnextclicked)
+        self.grid.attach(self.button1, 3, 19, 1, 1)
 
-		self.button2 = Gtk.Button.new_with_label("Exit")
-		self.button2.connect("clicked", self.onexitclicked)
-		self.grid.attach(self.button2, 1, 19, 1, 1)
+        self.button2 = Gtk.Button.new_with_label("Exit")
+        self.button2.connect("clicked", self.onexitclicked)
+        self.grid.attach(self.button2, 1, 19, 1, 1)
 
-	def onnextclicked(self,button):
-			exit(0)
-	def onexitclicked(self,button):
-			exit(1)
+    def onnextclicked(self,button):
+            exit(0)
+    def onexitclicked(self,button):
+            exit(1)
 
 
 def show_main():
-	window = main()
-	window.set_decorated(False)
-	window.set_resizable(False)
-	window.set_position(Gtk.WindowPosition.CENTER)
-	window.connect("delete-event", Gtk.main_quit)
-	window.show_all()
-	Gtk.main()
+    window = main()
+    window.set_decorated(False)
+    window.set_resizable(False)
+    window.set_position(Gtk.WindowPosition.CENTER)
+    window.connect("delete-event", Gtk.main_quit)
+    window.show_all()
+    Gtk.main()
 
 
 show_main()
--- usr/share/system-installer/modules/auto_login_set.py
+++ usr/share/system-installer/modules/auto_login_set.py
@@ -28,39 +28,39 @@ from os import remove
 
 # Make it easier for us to print to stderr
 def eprint(*args, **kwargs):
-	print(*args, file=stderr, **kwargs)
+    print(*args, file=stderr, **kwargs)
 
 
 def auto_login_set(LOGIN, USERNAME):
-	eprint("	###	auto_login_set.py started	###	")
-	new_conf = ""
-	with open("/etc/lightdm/lightdm.conf", "r") as conf:
-		new_conf = conf.read()
-	remove("/etc/lightdm/lightdm.conf")
-	new_conf = new_conf.split('\n')
-	for each in range(len(new_conf)):
-		if (each == 0):
-			continue
-		new_conf[each] = new_conf[each].split('=')
-	for each in range(len(new_conf)):
-		if (each == 0):
-			continue
-		if (new_conf[each][0] == "autologin-user"):
-			if ( (LOGIN == "0") or ( LOGIN == 0) or (LOGIN == False) ):
-				del(new_conf[each])
-			else:
-				new_conf[each][1] = USERNAME
-			break
-	for each in range(len(new_conf)):
-		if (each == 0):
-			continue
-		new_conf[each] = "=".join(new_conf[each])
-	with open("/etc/lightdm/lightdm.conf", "w+") as file:
-		for each in new_conf:
-			file.write(each)
-			file.write('\n')
-	eprint("	###	auto_login_set.py closed	###	")
+    eprint("    ### auto_login_set.py started   ### ")
+    new_conf = ""
+    with open("/etc/lightdm/lightdm.conf", "r") as conf:
+        new_conf = conf.read()
+    remove("/etc/lightdm/lightdm.conf")
+    new_conf = new_conf.split('\n')
+    for each in enumerate(new_conf):
+        if (each == 0):
+            continue
+        new_conf[each] = new_conf[each].split('=')
+    for each in enumerate(new_conf):
+        if (each == 0):
+            continue
+        if (new_conf[each][0] == "autologin-user"):
+            if ( (LOGIN == "0") or ( LOGIN == 0) or (LOGIN == False) ):
+                del(new_conf[each])
+            else:
+                new_conf[each][1] = USERNAME
+            break
+    for each in enumerate(new_conf):
+        if (each == 0):
+            continue
+        new_conf[each] = "=".join(new_conf[each])
+    with open("/etc/lightdm/lightdm.conf", "w+") as file:
+        for each in new_conf:
+            file.write(each)
+            file.write('\n')
+    eprint("    ### auto_login_set.py closed    ### ")
 
 if __name__ == '__main__':
-	auto_login_set(argv[1], argv[2])
+    auto_login_set(argv[1], argv[2])
 
