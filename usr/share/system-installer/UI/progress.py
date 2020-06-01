ea16e35570496982076da4c1a76f981b4b8378ac
Thomas Castleman <contact@draugeros.org>
2020-05-31 21:19:10 -0400

    fix text alignment on progress bar

--- usr/share/system-installer/UI/progress.py
+++ usr/share/system-installer/UI/progress.py
@@ -48,7 +48,7 @@ class Main(Gtk.ApplicationWindow):
 
         self.label = Gtk.Label()
         self.label.set_markup("""
-\t<b>Installing Drauger OS to your internal hard drive.</b>
+\t<b>Installing Drauger OS to your internal hard drive.</b>\t
 This may take some time. If you have an error, please send
 the log file (located at /tmp/system-installer.log)
 to: contact@draugeros.org   """)
