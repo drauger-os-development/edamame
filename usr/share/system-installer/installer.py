14068a51b4a62fb1b86976db04b201351ef486b0
Thomas Castleman <contact@draugeros.org>
2020-07-28 15:06:59 -0400

    copy live system network settings to installed system
    Saves the user time post-installation

--- usr/share/system-installer/installer.py
+++ usr/share/system-installer/installer.py
@@ -231,8 +231,13 @@ def install(settings):
         settings["UPDATES"] = False
     # ues check_call(["arch-chroot", "python3", "/master.py", ...]) because it
     # jumps through a lot of hoops for us.
-    # check_call(["arch-chroot", "python3", "/master.py", settings], stdout=stderr.buffer)
+    # check_call(["arch-chroot", "python3", "/master.py", settings],
+    # stdout=stderr.buffer)
     internet = modules.master.check_internet()
+    # Copy live system networking settings into installed system
+    rmtree("/mnt/etc/NetworkManager/system-connections")
+    copytree("/etc/NetworkManager/system-connections",
+             "/mnt/etc/NetworkManager/system-connections")
     real_root = chroot.arch_chroot("/mnt")
     modules.master.install(settings, internet)
     chroot.de_chroot(real_root, "/mnt")
