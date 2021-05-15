2ba3543f4c1c0adae6e8ef01c67618ea0c4bec6e
Thomas Castleman <contact@draugeros.org>
2021-05-15 19:31:44 -0400

    make sure all output is being sent to log file

--- usr/share/system-installer/modules/make_swap.py
+++ usr/share/system-installer/modules/make_swap.py
@@ -3,7 +3,7 @@
 #
 #  make_swap.py
 #
-#  Copyright 2020 Thomas Castleman <contact@draugeros.org>
+#  Copyright 2021 Thomas Castleman <contact@draugeros.org>
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
