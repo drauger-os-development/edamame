5b8237509206a2044c858768fe6c1a54ee27fd2a
Thomas Castleman <contact@draugeros.org>
2022-01-15 01:01:37 -0500

    I think this will do it

--- build.sh
+++ build.sh
@@ -109,7 +109,7 @@ base="$PWD"
 cd ..
 #DELETE STUFF HERE
 if [ "$OPTIONS" != "--pool" ]; then
-	rm "$base"/system-installer/usr/share/system-installer/modules/kernel.tar.xz
+	rm "$base"/usr/share/system-installer/modules/kernel.tar.xz
 fi
 # delete binary files from repo
 rm "$base"/usr/bin/system-installer
