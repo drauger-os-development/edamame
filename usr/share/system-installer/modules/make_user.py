c0f80d24da0eac17d028d368cfa4fe972657a8aa
Thomas Castleman <contact@draugeros.org>
2022-01-20 18:39:50 -0500

    fix a couple hidden bugs

--- usr/share/system-installer/modules/make_user.py
+++ usr/share/system-installer/modules/make_user.py
@@ -58,7 +58,11 @@ def make_user(username):
     new_home = "/home/" + username
     if os.path.exists(new_home):
         eprint("Original home folder found. Substituting it in . . .")
-        rmtree("/home/live")
+        try:
+            rmtree("/home/live")
+        except FileNotFoundError:
+            if username != "home": #literally a 1 in a trillion chance of happening, but just in case
+                rmtree("/home/home")
     elif os.path.exists("/home/home/live"):
         __fix_home__(username)
     else:
