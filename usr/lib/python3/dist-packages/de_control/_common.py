2885c92a159bcd148eec65c129bb73357f1ddd91
Thomas Castleman <contact@draugeros.org>
2023-11-01 19:44:15 -0400

    Minor update to DE support in de_control

---
+++ usr/lib/python3/dist-packages/de_control/_common.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  _common.py
+#
+#  Copyright 2023 Thomas Castleman <batcastle@draugeros.org>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+"""Common internal functions for de_control"""
+import os
+
+def get_de():
+    return os.getenv("XDG_CURRENT_DESKTOP").upper()
